ba2096191f1ac18875011edd9caf19efa98b5e9f
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2025-11-26 23:23:07 -0500

    Add initial FNG theme files and templates
    Introduces the new FNG Moodle theme with custom JavaScript for copy/paste prevention, PHP renderers extending RemUI, utility classes for theme settings, English and Spanish language packs, and a set of Mustache templates for core and RemUI layouts. This provides the foundation for FNG-specific UI, content protection, and theme configuration.

---
+++ theme/fng/lang/en/theme_fng.php
@@ -0,0 +1,179 @@
+<?php
+$string['pluginname'] = 'FNG';
+$string['choosereadme'] = 'FNG is a theme created by IngeWeb (<a href="http://ingeweb.co/">IngeWeb</a>) exclusively for the Fondo Nacional de Garantías.';
+
+$string['configtitle'] = 'FNG Theme';
+
+$string['logo'] = 'Logo';
+
+$string['themesettings'] = 'FNG Theme Settings';
+$string['themesettingsgeneral'] = 'General Settings';
+$string['contactinfo'] = 'Contact info';
+$string['abouttitle'] = 'About Title';
+$string['abouttitledesc'] = 'Title for "About" section in footer area';
+$string['abouttitle_default'] = 'About Fondo Nacional de Garantías';
+
+$string['abouttext'] = 'About Text';
+$string['abouttextdesc'] = 'Content for "About" section in footer area. Use this area to provide information about the Fondo Nacional de Garantías.';
+$string['focusmode']='Focus Mode';
+$string['loginfootertext'] = 'Login Footer';
+$string['loginfootertextdesc'] = 'Content for footer area in login page';
+
+$string['generalalert'] = 'General Alert';
+$string['generalalertdesc'] = 'This will display an alert on your frontpage to advise users of a particular event or situation.';
+
+$string['footertitle'] = 'Middle Footer Title';
+$string['footertitledesc'] = 'The title for the middle module in the footer area.';
+
+$string['footertext'] = 'Middle Footer Text';
+$string['footertextdesc'] = 'The content for the middle module in the footer area.';
+
+$string['footertitle2'] = 'Right Footer Title';
+$string['footertitledesc2'] = 'The title for the right module in the footer area.';
+
+$string['footertext2'] = 'Right Footer Text';
+$string['footertextdesc2'] = 'The content for the right module in the footer area.';
+
+$string['copyright'] = 'Copyright';
+$string['copyrightdesc'] = 'About this website.';
+
+$string['customcss'] = 'Custom CSS';
+$string['customcssdesc'] = 'Whatever CSS rules you add to this area will be reflected in every page, making for easier customisation of this theme.';
+
+$string['themeinfotext'] = 'This theme was created for <strong>Fondo Nacional de Garantías</strong> by <a target="_blank" href="http://ingeweb.co/">IngeWeb - Soluciones para triunfar en Internet</a>.';
+$string['credit'] = 'Copyright © 2025 Fondo Nacional de Garantías - All rights reserved';
+
+$string['showingonlycategorieswithenrolledcourses'] = 'Showing categories containing enrolled courses only';
+$string['nocoursestoshow'] = 'No courses to show were found';
+
+$string['region-side-pre'] = 'Right';
+
+// Copy/Paste functionality
+$string['copypaste'] = 'Copy and Paste';
+$string['copypaste_desc'] = 'Enable or disable copy & paste for students';
+$string['config_copypaste'] = 'Enable or disable copy & paste';
+$string['config_copypaste_desc'] = 'Enable or disable copy & paste for students';
+$string['disable'] = 'Disable';
+$string['enable'] = 'Enable';
+
+$string['show'] = 'Show';
+$string['hide'] = 'Hide';
+
+// Carousel strings
+$string['carouselsettings'] = 'Login Carousel Settings';
+$string['carouselsettings_desc'] = 'Configure the carousel on the login page including images, titles, and links. This creates an engaging visual experience for users logging in.';
+
+$string['enablecarousel'] = 'Enable login page carousel';
+$string['enablecarousel_desc'] = 'Enable this setting to display a carousel of images on the login page.';
+
+$string['numberofslides'] = 'Number of slides';
+$string['numberofslides_desc'] = 'Select how many slides you want to display in the login page carousel. Each slide can be individually configured.';
+
+$string['slideheading'] = 'Slide {$a} Settings';
+$string['slideheading_desc'] = 'Configure all settings for slide {$a} of the login carousel.';
+
+$string['slideshowtitle'] = 'Show title for slide {$a}';
+$string['slideshowtitle_desc'] = 'Check this option if you want to display a title caption for this slide.';
+
+$string['slideenableurl'] = 'Enable link for slide {$a}';
+$string['slideenableurl_desc'] = 'Check this option if you want this slide to be clickable and link to a URL.';
+
+$string['slidetitle'] = 'Title for slide {$a}';
+$string['slidetitledesc'] = 'Enter the caption text that will appear overlay on this slide. Only visible if "Show title" is enabled.';
+
+$string['slideurl'] = 'Slide {$a} Button URL';
+$string['slideurldesc'] = 'Enter the URL that this slide will link to when clicked. Only active if "Enable link" is enabled.';
+
+$string['slideimage'] = 'Slide {$a} image';
+$string['slideimage_desc'] = 'Upload an image for slide {$a} in the carousel. Recommended size: 1920×1080px or larger with the same aspect ratio.';
+
+$string['carouselinterval'] = 'Carousel Interval';
+$string['carouselintervaldesc'] = 'Set the time interval in milliseconds between slide transitions. A value of 5000 equals 5 seconds between slides.';
+
+$string['default_slide_title'] = 'Welcome to Fondo Nacional de Garantías';
+
+// Login page carousel
+$string['carousel'] = 'Login Page Carousel';
+$string['loginsection'] = 'Login section';
+$string['previous'] = 'Go to the previous slide';
+$string['next'] = 'Go to the next slide';
+
+// URL validation
+$string['unauthorized_access'] = 'Unauthorized Access';
+$string['unauthorized_access_msg'] = 'This site is not authorized to operate at this URL.';
+$string['devtools_access_disabled'] = 'Access to the development tools is disabled.';
+
+// Visibility settings
+$string['hidefrontpagesections'] = 'Hide front page top sections';
+$string['hidefrontpagesections_desc'] = 'Enable this setting to hide the top sections of your front page, providing a cleaner look.';
+
+$string['hidefootersections'] = 'Hide footer sections';
+$string['hidefootersections_desc'] = 'Enable this setting to hide sections of your footer, providing a cleaner look.';
+
+// Header images
+$string['themesettingslogin'] = 'Basic login settings';
+$string['personalareaheader'] = 'Personal Area Header Image';
+$string['personalareaheaderdesc'] = 'Upload an image that will be displayed as the header in the personal area.';
+$string['mycoursesheader'] = 'My Courses Header Image';
+$string['mycoursesheaderdesc'] = 'Upload an image that will be displayed as the header on the my courses page.';
+
+// Cache definition
+$string['cachedef_fontawesomeremuiiconmapping'] = 'Cache Storage for Font Awesome';
+
+// New strings for the updated settings.php
+$string['generalsettings'] = 'General Settings';
+$string['generalnotice'] = 'General Notice';
+$string['generalnoticedesc'] = 'Enter a sitewide notice that will be displayed at the top of your site. This is useful for important announcements, maintenance information, or other critical messages.';
+$string['generalnotice_create'] = 'Create a Notice';
+$string['generalnoticeheading'] = 'General Notice Settings';
+$string['generalnoticemode'] = 'General Notice Mode';
+$string['generalnoticemodedesc'] = 'Choose how to display your general notice. Different modes use different styling to indicate the importance of the message.';
+$string['generalnoticemode_off'] = 'Off - No notice displayed';
+$string['generalnoticemode_info'] = 'Information - Blue informational notice';
+$string['generalnoticemode_danger'] = 'Critical Alert - Red warning notice';
+
+$string['chatheading'] = 'Live Chat Settings';
+$string['enable_chat'] = 'Enable Live Chat';
+$string['enable_chatdesc'] = 'Enable live chat support for logged-in users. This feature allows real-time communication between users and support staff.';
+$string['tawkto_embed_url'] = 'Tawk.to Embed URL';
+$string['tawkto_embed_urldesc'] = 'Enter your complete Tawk.to widget URL. You can obtain this from your Tawk.to dashboard after creating an account and setting up your chat widget.';
+
+$string['accessibilityheading'] = 'Accessibility Settings';
+$string['accessibility_widget'] = 'Accessibility Widget';
+$string['accessibility_widgetdesc'] = 'Enable an accessibility widget that allows users to customize their viewing experience with features like text size adjustment, contrast modes, and other accessibility options.';
+
+$string['contentprotectionheading'] = 'Content Protection Settings';
+$string['copypaste_prevention'] = 'Prevent Copy/Paste';
+$string['copypaste_preventiondesc'] = 'Enable copy and paste prevention features to protect course content from unauthorized copying. This can be applied to specific user roles.';
+$string['copypaste_roles'] = 'Protected Roles';
+$string['copypaste_rolesdesc'] = 'Select which user roles will have copy/paste restrictions applied. Only users with these roles will be affected by the restrictions.';
+
+$string['loginsliderheading'] = 'Login Slider Settings';
+$string['slideheading'] = 'Slide {$a}';
+
+$string['dashboardsettings'] = 'Dashboard Settings';
+$string['personalareaheading'] = 'Personal Area Settings';
+$string['mycoursesheading'] = 'My Courses Settings';
+$string['show_personalareaheader'] = 'Show Personal Area Header';
+$string['show_personalareaheaderdesc'] = 'Show or hide the personal area header image';
+$string['show_mycoursesheader'] = 'Show My Courses Header';
+$string['show_mycoursesheaderdesc'] = 'Show or hide the my courses header image';
+
+$string['footersettings'] = 'Footer Settings';
+$string['footeraboutheading'] = 'Footer About Section';
+
+$string['loginsettings'] = 'Login Settings';
+
+// Keep the default about text
+$string['abouttext_default'] = '<p style="text-align: center;"></p>
+    <p style="text-align: left;">
+    </p>
+    <p style="text-align: center;"><span style="font-family: inherit; font-size: 1rem; letter-spacing: 0rem; text-transform: inherit;"><b>Fondo Nacional de Garantías<br></b></span><span style="font-family: inherit; font-size: 1rem; letter-spacing: 0rem; text-transform: inherit;">Facilitating access to credit for small businesses in Colombia<br></span><strong style="font-family: inherit; font-size: 1rem; letter-spacing: 0rem; text-transform: inherit;">www.fng.gov.co</strong></p>
+    <p></p>';
+
+$string['fnghelp'] = "Platform Help";
+$string['fngfeedback'] = "FNG Feedback";
+$string['fngsupport'] = "FNG Support";
+
+$string['enrolleduserscountvisibilityhead'] = "Show 'Enrolled students' information";
+$string['enrolleduserscountvisibilitydesc'] = "Disable to hide the 'Enrolled students' information";
